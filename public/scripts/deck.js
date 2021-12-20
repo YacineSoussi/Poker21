2f92e51e032a0dd1a07216b7c357fd65a758669b
Júlio César Peixoto Pereira <julioocesarr@outlook.fr>
2021-12-20 12:21:11 +0100

    Implement deck class

--- public/scripts/deck.js
+++ public/scripts/deck.js
@@ -1,5 +1,57 @@
 class Deck {
     constructor() {
+        this.cards = [];
+    }
 
+    /**
+     * Get cards data
+     * - { cards, deck_id, remaining, success }
+     * @returns Cards properties
+     */
+    async getCardsData() {
+        let result;
+
+        await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Error during get deck object")
+                }
+            })
+            .then(data => {
+                result = fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=52`)
+                            .then(response => {
+                                if(response.ok) {
+                                    return response.json();
+                                } else {
+                                    throw new Error("Error during get deck data")
+                                }
+                            })
+                            .catch(error => {
+                                throw new Error(error);
+                            });
+            })
+            .catch(error => {
+                throw new Error(error);
+            })
+
+        return result;
+    }
+
+    /**
+     * Get cards
+     * @returns Cards array
+     */
+    getCards() {
+        return this.cards;
+    }
+
+    /**
+     * Set cards
+     * @param {array} cards Cards array
+     */
+    setCards(cards) {
+        this.cards = cards;
     }
 }
